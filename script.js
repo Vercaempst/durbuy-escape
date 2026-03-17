73205960d320d4d12b5980f890e12701b5f5c4f2
Vercaempst <43634630+Vercaempst@users.noreply.github.com>
2026-03-17 18:59:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ import {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxS4ClC6ZL6fJki5jHx8uWUy-1MQxqfZGQm50Ri7pzes-n23ZANHy6pxTl9lCCn3lBq/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzvFlESGMqtYAbYMtRtCJhL511GNhRLUquYGcktW-2P7frf8Ay0b13IW0ZKmXRq8s2M/exec";
 
 let map;
 let playerMarker;
