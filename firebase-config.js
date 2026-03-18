8e36e21277b311a5340444d1baa62c2ef130582e
Vercaempst <43634630+Vercaempst@users.noreply.github.com>
2026-03-18 17:17:54 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDSblhmRLoIMFMtGlBMGZo-_HKSnGOMLWM",
-    authDomain: "durbuy-escape.firebaseapp.com",
-    projectId: "durbuy-escape",
-    storageBucket: "durbuy-escape.firebasestorage.app",
-    messagingSenderId: "1023625652672",
-    appId: "1:1023625652672:web:28d79da17c2bfa8e654025",
-    measurementId: "G-3GDM1HQ117",
-    databaseURL: "https://durbuy-escape-default-rtdb.europe-west1.firebasedatabase.app"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDSblhmRLoIMFMtGlBMGZo-_HKSnGOMLWM",
+  authDomain: "durbuy-escape.firebaseapp.com",
+  databaseURL: "https://durbuy-escape-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "durbuy-escape",
+  storageBucket: "durbuy-escape.firebasestorage.app",
+  messagingSenderId: "1023625652672",
+  appId: "1:1023625652672:web:28d79da17c2bfa8e654025",
+  measurementId: "G-3GDM1HQ117"
+};
+
+firebase.initializeApp(firebaseConfig);
